f3d08dcabf44abe77dc4a5192b76d2e3a315676b
Dao Linh <luyendh@fpt.edu.vn>
2019-09-18 21:20:05 +0700

    Hello T1807E

---
+++ T1807EHelloUWP/Pages/Register.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json;
+using T1807EHelloUWP.Entity;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace T1807EHelloUWP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Register : Page
+    {
+        public Register()
+        {
+            this.InitializeComponent();
+        }
+        
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var member = new Member
+            {
+                Username = this.Username.Text,
+                Password = this.Password.Password
+            };
+            // validate phía client.
+            Debug.WriteLine(JsonConvert.SerializeObject(member));
+            HttpClient httpClient = new HttpClient();
+        }
+    }
+
+}
