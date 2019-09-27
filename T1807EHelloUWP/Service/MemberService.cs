5ba908d1b2e8a338eea6d983d982d34c7802827a
Dao Linh <luyendh@fpt.edu.vn>
2019-09-27 21:11:11 +0700

    Implement clean code.

---
+++ T1807EHelloUWP/Service/MemberService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T1807EHelloUWP.Entity;
+
+namespace T1807EHelloUWP.Service
+{
+    interface MemberService
+    {
+        String Login(String username, String password);
+
+        Member Register(Member member);
+
+        Member GetInformation(String token);
+    }
+}
