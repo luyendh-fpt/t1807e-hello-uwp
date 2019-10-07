3f4a901bc243064bc3e77316767346842cfa5a3e
Dao Linh <luyendh@fpt.edu.vn>
2019-10-07 20:14:11 +0700

    Demo flow with service and view, get list free song.

--- T1807EHelloUWP/Constant/ApiUrl.cs
+++ T1807EHelloUWP/Constant/ApiUrl.cs
@@ -8,10 +8,9 @@ namespace T1807EHelloUWP.Constant
 {
     class ApiUrl
     {
-        public const string API_BASE_URL = "https://2-dot-backup-server-003.appspot.com/_api/v2/members";
-        public const string LOGIN_URL = API_BASE_URL + "/authentication";
-        public const string GET_INFORMATION_URL = API_BASE_URL + "/information";
-        
-
+        public const string API_BASE_URL = "https://2-dot-backup-server-003.appspot.com/_api/v2";
+        public const string LOGIN_URL = API_BASE_URL + "/members/authentication";
+        public const string GET_INFORMATION_URL = API_BASE_URL + "/members/information";
+        public const string GET_FREE_SONG_URL = API_BASE_URL + "/songs/get-free-songs";
     }
 }
---
+++ T1807EHelloUWP/Pages/ListSong.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using T1807EHelloUWP.Entity;
+using T1807EHelloUWP.Service;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace T1807EHelloUWP.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ListSong : Page
+    {
+        private ISongService songService;
+        ObservableCollection<Song> Songs { get; set; }
+
+        public ListSong()
+        {
+            this.InitializeComponent();
+            songService = new SongService();
+            LoadSongs();
+        }
+
+        private void LoadSongs()
+        {
+            Songs = songService.GetFreeSongs();
+        }
+    }
+}
